74a7b7f55fbbdbd09a9973f2134f9babfcebd4c9
Gian <gianperez027@gmail.com>
2026-05-20 00:48:56 +0800

    fix2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  root: './',
+  root: resolve(__dirname, './'),
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
   },
   server: {
     port: 3000,
     strictPort: false,
+    open: '/',
   }
 })
