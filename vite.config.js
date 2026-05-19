7cd5627ea16f9e9c502a254ba076e2824e58e128
Gian <gianperez027@gmail.com>
2026-05-20 00:51:50 +0800

    Simplify Vite and Render configuration

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,6 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-
-export default defineConfig({
-  root: resolve(__dirname, './'),
+export default {
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-  server: {
-    port: 3000,
-    strictPort: false,
-    open: '/',
   }
-})
+}
